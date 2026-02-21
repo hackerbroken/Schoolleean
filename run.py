d9f1ce62cefca8df7c21ee9ed5d449ef4132350b
hackerbroken <162806669+hackerbroken@users.noreply.github.com>
2026-02-21 18:15:51 +0530

    Add files via upload

---
+++ run.py
@@ -0,0 +1,8 @@
+import os
+from app import app
+
+
+if __name__ == '__main__':
+    os.makedirs('templates', exist_ok=True)
+    os.makedirs('static', exist_ok=True)
+    app.run(debug=True, port=5000)
